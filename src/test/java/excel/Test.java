fee40de1835937d7007fd41a96db45339fba2bde
lim <lim@digitalgd.com.cn>
2019-11-20 00:32:46 +0800

    change the algorithm of fit

--- src/test/java/excel/Test.java
+++ src/test/java/excel/Test.java
@@ -20,7 +20,7 @@ public class Test {
 
         DecimalFormat df = new DecimalFormat("0.000000000000");
 
-        List<List<String>> rs = new ArrayList<>();
+        /*List<List<String>> rs = new ArrayList<>();
 
         for (List<Object> row : readAll) {
             List<String> r = new ArrayList<>();
@@ -46,9 +46,61 @@ public class Test {
             r.add(df.format(c));
 
             rs.add(r);
+        }*/
+
+        List<List<Double>> rs = new ArrayList<>();
+
+        for (List<Object> row : readAll) {
+            List<Double> r = new ArrayList<>();
+            Double a = (Double) row.get(0);
+            Double b = (Double) row.get(1);
+            Double c = (Double) row.get(2);
+
+            r.add(a);
+            if(a < 2.6) {
+                b *= 1.1;
+            } else if(a < 2.7) {
+                b *= 1.12;
+            } else if (a < 2.8) {
+                b *= 1.15;
+            } else if (a < 2.9) {
+                b *= 1.2;
+            } else if (a < 3.0) {
+                b *= 1.25;
+            } else if(a < 3.05) {
+                b *= 1.3;
+            } else if (a < 3.1) {
+                b *= (1.35 + (a - 3.1) * 0.1);
+            } else if (a < 3.15) {
+                b *= (1.4 + (a - 3.15) * 0.1);
+            } else if (a < 3.2) {
+                b *= (1.45 + (a - 3.2)* 0.1);
+            } else if (a < 3.25) {
+                b *= (1.5 + (a - 3.25)* 0.1);
+            } else if (a < 3.3) {
+                b *= (1.5 + (a - 3.3)* 0.1);
+            }else if (a < 3.35) {
+                b *= (1.45 + (a - 3.35)* 0.1);
+            } else if (a < 3.4) {
+                b *= (1.4 + (a - 3.4)* 0.1);
+            } else if (a < 3.45) {
+                b *= (1.35 + (a - 3.45)* 0.1);
+            } else if (a < 3.5) {
+                b *= (1.35 + (a - 3.5)* 0.1);
+            } else if (a < 3.55) {
+                b *= (1.3 + (a - 3.55)* 0.1);
+            } else if (a < 3.6) {
+                b *= (1.3 + (a - 3.6)* 0.1);
+            } else if (a < 3.65) {
+                b *= (1.255 + (a - 3.65)* 0.1);
+            }
+            r.add(b);
+            r.add(c);
+
+            rs.add(r);
         }
 
-        System.out.println(rs);
+//        System.out.println(rs);
 
         //通过工具类创建writer
         ExcelWriter writer = ExcelUtil.getWriter("d:/data3.xlsx");
