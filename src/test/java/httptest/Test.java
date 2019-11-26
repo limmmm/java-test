1d847326979a1762e512728174f88a55d0b274b0
lim <lim@digitalgd.com.cn>
2019-11-27 00:04:07 +0800

    change the algorithm of fit

--- src/test/java/httptest/Test.java
+++ src/test/java/httptest/Test.java
@@ -4,6 +4,7 @@ import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.Date;
 import java.util.HashMap;
 
 /**
@@ -12,7 +13,44 @@ import java.util.HashMap;
  */
 public class Test {
     public static void main(String[] args) {
-        for(int i=0; i<1; i++) {
+
+
+        for(int t=0; t < 100000; t++) {
+            long time = new Date().getTime();
+            System.out.println(time);
+
+            HashMap<String, Object> paramMap = new HashMap<>();
+            paramMap.put("zone", "国内");
+            paramMap.put("telephone", "1234567890");
+            paramMap.put("name", "游客" + time);
+
+            String url = "http://minisite.medtoday.com.cn/cycle/v-dmkang-physician/user_register.php";
+            String result = HttpUtil.post(url, paramMap);
+            JSONObject rj = JSONObject.parseObject(result);
+            JSONObject user = (JSONObject) rj.getJSONArray("user").get(0);
+            String userId = user.getString("user_id");
+            System.out.println(userId);
+
+            for(int i=0; i<10; i++) {
+                String url_vote = "http://minisite.medtoday.com.cn/cycle/v-dmkang-physician/updatefileupqtybyfileid.php";
+                HashMap<String, Object> paramMap2 = new HashMap<>();
+                paramMap2.put("file_id", "565");
+                paramMap2.put("user_id", userId);
+                paramMap2.put("user_type_id", "1");
+                String result2 = HttpUtil.post(url_vote, paramMap);
+                System.out.println(result2);
+            }
+
+//            int k=5000;
+//            while(k-- > 0){
+//
+//            };
+//            long s = 100;
+//            Thread.sleep(s);
+        }
+
+
+        /*for(int i=0; i<1; i++) {
             String result1= HttpUtil.get("http://minisite.medtoday.com.cn/cycle/wechat/configforlive/wxconfig.php?url=http%3A%2F%2Fminisite.medtoday.com.cn%2FH5%2F1127poa%2Findex.html");
             JSONObject re = JSONObject.parseObject(result1);
             String key = re.getJSONObject("signPackage").getString("signature");
@@ -33,6 +71,6 @@ public class Test {
 //                String result= HttpRequest.post(u).execute().body();
 //                System.out.println(result);
             }
-        }
+        }*/
     }
 }
